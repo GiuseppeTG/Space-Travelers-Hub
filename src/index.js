f668ca4fc4b9cac42051c2bfad874084c8541269
Giuseppe Tomasini <giusetomasini@gmail.com>
2022-07-16 12:13:59 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './redux/configureStore';
+import App from './App';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+);
