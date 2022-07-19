d3bdd35b4e6c7f71c94788067b50302d369d4f42
Fabien <fabienswift@gmail.com>
2022-07-19 16:48:38 -0400

    Add

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import rocketsReducer from './Rockets/Rockets';
+import missionsReducer from './Missions/Missions';
+
+const store = configureStore({
+  reducer: {
+    rockets: rocketsReducer,
+    missions: missionsReducer,
+  },
+});
+
+export default store;
